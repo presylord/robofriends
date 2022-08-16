dbd47e637ce6143f72dbed2b47fa1cac4094d14c
presylord <kidlat123@gmail.com>
2022-08-16 19:35:42 +0800

    init

---
+++ src/CardList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {robots.map((user, index) => {
+        return (
+          <Card
+            key={index}
+            id={user.id}
+            name={user.name}
+            username={user.username}
+            email={user.email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default CardList;
